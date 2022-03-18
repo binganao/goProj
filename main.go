cc20d2261f1316fb6e654c57e5087d8fbe503aaa
wmillers <31211860+wmillers@users.noreply.github.com>
2022-03-18 18:42:08 +0800

    add prepend slice

--- main.go
+++ main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
-	//"io/uiutil"
-	//"net/http"
-	//"os"
-	//"strconv"
-	"time"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	defer func(start time.Time) {
-		fmt.Println("Total use:\t", time.Since(start))
-	}(time.Now())
-	a := func() {
-		fmt.Println(1)
-	}
-	go a()
-	fmt.Println(2)
+func prepend(slice []string, ele string) []string {
+	return append([]string{ele}, slice...)
+}
 
-	b := map[string]int{}
-	b["asd"] = 1
-	o, k := b["s"]
-	fmt.Println(b["asd"], b["aas"], o, k, len(b))
+func main() {
+	var n string
+	var sum int
+	fmt.Println(strings.Join([]string{"1", "2"}, "--"))
+	fmt.Scanf("%s", &n)
+	for i := 0; i < len(n); i++ {
+		place, _ := strconv.Atoi(string(n[i]))
+		sum += place
+	}
+	res := []string{""}
+	py := []string{"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"}
+	for ; sum > 0; sum /= 10 {
+		res = prepend(res, py[sum%10])
+	}
+	for i := 0; i < len(res); i++ {
+		if i != 0 && i != len(res)-1 {
+			fmt.Print(" ")
+		}
+		fmt.Print(res[i])
+	}
 }
